6fbf3e78c525aeb869caca801458f16c192293d2
圆圆的肚子 <2184363879@qq.com>
2022-01-17 20:47:37 +0800

    Initial commit

---
+++ sport/src/main/java/com/example/sport/sport_MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sport;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sport_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sport_activity_main);
+    }
+}
